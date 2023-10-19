4e11e41fc7cb64e8898e9326b0d3ef86f11ec8a3
BartoszewskiA <bartoszewski.a@outlook.com>
2023-10-19 12:26:40 +0200

    19.10,2023

--- lab02p01/wynik.php
+++ lab02p01/wynik.php
@@ -1,13 +1,52 @@
 <!doctype html>
 <html>
-  <head>
-    <meta charset="UTF-8" />
-    <title></title>
-  </head>
-  <body>
-    <h2>Podsumowanie:</h2>
-    <?php
-    
-    ?>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <title></title>
+</head>
+
+<body>
+  <h2>Podsumowanie:</h2>
+  <?php
+  if (!isset($_POST["imie"]) || !isset($_POST["nazwisko"]) || !isset($_POST["semestr"])) {
+    echo "Brakuje niezbędnych danych <br>";
+    echo "<a href='index.html'>Powrot do formularza</a>";
+    return;
+  }
+
+  if (empty($_POST["imie"]) || empty($_POST["nazwisko"]) || empty($_POST["semestr"])) {
+    echo "Dane nie mogą być puste <br>";
+    echo "<a href='#' onclick='window.history.go(-1);'>Powrot do formularza</a>";
+    return;
+  }
+
+  if (!is_numeric($semestr) || intval($semestr) > 7 || intval($semestr) < 1) {
+    echo "Nieprawidłowy numer semestru, musi być liczbą z przedziału od 1 do 7. <br>";
+    echo "<a href='index.html'>Powrot do formularza</a>";
+    return;
+  }
+
+  $imie = $_POST["imie"];
+  $nazwisko = $_POST["nazwisko"];
+  $semestr = $_POST["semestr"];
+  $stan_zaliczenia_jako_string = "";
+
+
+
+  if (isset($_POST["zaliczone"])) {
+    $stan_zaliczenia_jako_string = "Zaliczony";
+  } else {
+    $stan_zaliczenia_jako_string = "Niezaliczony";
+  }
+
+
+  echo "Student semestru $semestr <br>";
+  echo "Imię: $imie <br>";
+  echo "Nazwisko: $nazwisko <br>";
+  echo "Semestr $semestr $stan_zaliczenia_jako_string <br>";
+
+  ?>
+</body>
+
 </html>
